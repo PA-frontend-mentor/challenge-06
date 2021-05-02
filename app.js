4358cf7a31dd4587a4fb152c5411790cb08eb9e6
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-05-02 19:57:22 +0430

    [DEL] all css + html

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const arrow = document.getElementsByClassName("arrow");
 const shareBoxParent = document.getElementsByClassName("share-wrapper");
 
 shareBtn[0].addEventListener("click", () => clickHandler());
+shareBtn[0].addEventListener("mouseenter", () => clickHandler());
+shareBoxParent[0].addEventListener("mouseleave", () => mouseLeave());
 
 const clickHandler = () => {
   console.log(shareBox[0].classList.length);
@@ -16,10 +18,19 @@ const clickHandler = () => {
   }
 };
 
+const mouseLeave = () => {
+  if (shareBox[0].classList.length == 1) {
+    shareBox[0].className += " hidden";
+    rectangular[0].className += " hidden";
+    shareBtn[0].classList = shareBtn[0].classList[0];
+    arrow[0].classList = arrow[0].classList[0];
+  }
+};
+
 document.addEventListener("click", (e) => {
   let isInside = shareBoxParent[0].contains(e.target);
 
-  if (!isInside && !shareBtn[0].contains(e.target)) {
+  if (!isInside && !shareBox[0].contains(e.target) && shareBox[0].classList.length == 1) {
     shareBox[0].className += " hidden";
     rectangular[0].className += " hidden";
     shareBtn[0].classList = shareBtn[0].classList[0];
